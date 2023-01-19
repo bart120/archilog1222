b5188ed512fc33ccc6f5c4ab2d7df69d1597a1b7
Vincent Leclerc <vleclerc@inow.fr>
2023-01-19 10:19:58 +0100

    add tests

---
+++ APIExample/TestAPI/Data/MockDbContext.cs
@@ -0,0 +1,33 @@
+﻿using APIExample.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAPI.Data
+{
+    public class MockDbContext : ArchiDbContext
+    {
+        public MockDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public static MockDbContext GetDbContext(bool withData = true)
+        {
+            var options = new DbContextOptionsBuilder().UseInMemoryDatabase("dbtest").Options;
+            var dbContext = new MockDbContext(options);
+
+            if (withData)
+            {
+                dbContext.Users.Add(new APIExample.Models.User { Active= true, Name = "Leponge", FirstName = "Bob", BirthDate = DateTime.Now });
+                dbContext.Users.Add(new APIExample.Models.User { Active = true, Name = "Leponge1", FirstName = "Bob1", BirthDate = DateTime.Now });
+                dbContext.Users.Add(new APIExample.Models.User { Active = true, Name = "Leponge2", FirstName = "Bob2", BirthDate = DateTime.Now });
+                dbContext.SaveChanges();
+            }
+
+            return dbContext;
+        }
+    }
+}
---
+++ APIExample/TestAPI/UsersControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using APIExample.Controllers;
+using APIExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TestAPI.Data;
+
+namespace TestAPI
+{
+    public class UsersControllerTests
+    {
+        private UsersController _usersController;
+        private MockDbContext _dbContext;
+         
+
+        [SetUp]
+        public void Setup()
+        {
+            _dbContext = MockDbContext.GetDbContext();
+            _usersController = new UsersController(_dbContext);
+        }
+
+        [Test]
+        public async Task TestGetUsers()
+        {
+            var actionResult = await _usersController.GetUsers();
+           // var result = actionResult.Result as ObjectResult;
+            var values = actionResult.Value as IEnumerable<User>;
+
+            //validation des tests
+            //Assert.AreEqual((int)HttpStatusCode.OK, result.StatusCode);
+            Assert.IsNotNull(values);
+            Assert.AreEqual(_dbContext.Users.Count(), values.Count());
+
+        }
+
+        [Test]
+        public async Task TestPostUser()
+        {
+            var user = new User { Active = true, Name = "Prost", FirstName = "Alain", BirthDate= DateTime.Now };
+            var actionResult = await _usersController.PostUser(user);
+
+            
+            //Assert.Positive(value.ID, "L'utilisateur n'a pas d'ID");
+
+            var userTest  = _dbContext.Users.SingleOrDefault(x => x.Name == "Prost");
+            Assert.IsNotNull(userTest);
+
+        }
+
+    }
+}
